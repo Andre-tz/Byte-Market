3257c0b1d4f17c1f40454cfcec7843401caa5e5f
Andre-tz <italo.tenorio1999@gmail.com>
2026-02-24 19:19:57 -0500

    feat: setup initial layout with Header, Footer and Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-const App = ()=>{
-    return (
-        <h1 className="bg-red-700">Hola, bienvenido a ByteMarket</h1>
-    )
-}
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div className="bg-orange-500 h-screen">
+        <header>
+            <Header/>
+        </header>
+        <main></main>
+        <footer>
+            <Footer/>
+        </footer>
+    </div>
+  );
+};
 
 export default App;
